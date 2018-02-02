0e2a12a81cfe840dde40f69aba02733d36155980
pixelherodev <pixelherodev@outlook.com>
2018-02-01 21:23:27 -0500

    Fix liblib detection so custom location can be specified

--- cmake_modules/FindLIBLIB.cmake
+++ cmake_modules/FindLIBLIB.cmake
@@ -1,5 +1,24 @@
-find_path(LIBLIB_INCLUDE_DIR liblib/liblib.hpp)
-find_library(LIBLIB_LIBRARY NAMES lib liblib)
+set(LIBLIB_PATHS
+	${LIBLIB_ROOT}
+	$ENV{LIBLIB_ROOT}
+	~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /sw
+    /opt/local
+    /opt/csw
+    /opt
+)
+
+find_path(LIBLIB_INCLUDE_DIR liblib/liblib.hpp
+	PATH_SUFFIXES include
+	PATHS ${LIBLIB_PATHS}
+)
+find_library(LIBLIB_LIBRARY NAMES lib liblib
+	PATH_SUFFIXES include
+	PATHS ${FIND_SFML_PATHS}
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBLIB  DEFAULT_MSG
