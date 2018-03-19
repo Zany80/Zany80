40a94652ed7d5214c4bb1e45e5d24dbf424e6ebc
pixelherodev <pixelherodev@outlook.com>
2018-03-18 22:02:58 -0400

    Switch to Win32, prevent repeat builds

--- cmake_modules/MINGW.cmake
+++ cmake_modules/MINGW.cmake
@@ -1,12 +1,12 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
 
-set(CMAKE_MODULE_PATH /usr/x86_64-w64-mingw32/cmake/Modules/ ${CMAKE_MODULE_PATH} )
+set(CMAKE_MODULE_PATH /usr/i686-w64-mingw32/cmake/Modules/ ${CMAKE_MODULE_PATH} )
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
