753ff04ee809266862c40bc0a49e705ad89219ba
Noam Preil <noam@umate-usb.home>
2018-04-02 13:38:50 -0400

    Attempt fix

--- cmake_modules/FindSFML.cmake
+++ cmake_modules/FindSFML.cmake
@@ -311,7 +311,7 @@ if(SFML_STATIC_LIBRARIES)
 
         # find libraries
         find_sfml_dependency(FREETYPE_LIBRARY "FreeType" freetype)
-        find_sfml_dependency(JPEG_LIBRARY "libjpeg" jpeg)
+#        find_sfml_dependency(JPEG_LIBRARY "libjpeg" jpeg)
 
         # update the list
         set(SFML_GRAPHICS_DEPENDENCIES ${FREETYPE_LIBRARY} ${JPEG_LIBRARY})
