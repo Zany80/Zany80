9530daf0c91161491d8952db7134a349e91f0107
Noam Preil <pleasantatk@gmail.com>
2018-12-22 23:16:26 -0500

    More repo location fixes

--- tools/publish.sh
+++ tools/publish.sh
@@ -5,6 +5,6 @@ git config --global push.default simple
 pushd ..
 git clone https://pixelherodev:$GITHUB_KEY@github.com/zany80/zany80.github.io || exit 1
 cd zany80.github.io
-export BUILDS=Oryolized
+export BUILDS=Zany80
 yes | ./update_zanyonline.sh
 popd
