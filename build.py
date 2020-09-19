2b81b98ee11efea55c623f1403768ace2cb2a88d
Noam Preil <pleasantatk@gmail.com>
2020-09-19 00:36:58 -0400

    extend build.py

--- build.py
+++ build.py
@@ -1,8 +1,63 @@
 #!/usr/bin/env python3
 
+import glob
+import os
+import argparse
+from pathlib import Path
+from string import Template
+import itertools
+import subprocess
+from tempfile import NamedTemporaryFile
+import multiprocessing
+
+# should be fine to just hardcode these, IMO
+
 MAJOR_VER=0
 MINOR_VER=2
 PATCH_VER=0
+PROJECT_NAME="Zany80"
+
+BUILD_DIR = 'build'
+BIN_DIR = BUILD_DIR
+OBJ_DIR = os.path.join(BUILD_DIR, 'obj')
+EXE_EXT = '.exe' if os.name == 'nt' else ''
+BIN_NAME = PROJECT_NAME + EXE_EXT
+
+DEFAULT_FLAGS = {'-Wall', '-pedantic', '-DSOKOL_GLCORE33', '-D_POSIX_C_SOURCE=200809L' }
+DEBUG_FLAGS = {'-g', '-Og', '-D_DEBUG'}
+# pixelherodev's personal flag set :P I'm insane, I know.
+PIXELS_DEVEL_FLAGS = { '-Werror', '-Wextra', '-Wno-error=reorder', '-Wno-error=pedantic', '-Wno-error=unused-parameter', '-Wno-error=missing-field-initializers', '-Wno-error=deprecated-declarations', '-pedantic', '-march=native', '-mtune=native', '-falign-functions=32' }
+RELEASE_FLAGS = {'-O2'}
+
+# these *do* need to be in order, so no sets!
+BASE_LDFLAGS = [ '-lX11', '-lXi', '-lXcursor', '-lGL', '-ldl', '-lstdc++', '-lm', '-pthread' ]
+DEFAULT_LDFLAGS = ['-static', '-static-libgcc' ] + BASE_LDFLAGS
+
+# parse arguments
+parser = argparse.ArgumentParser(description='Builds ' + PROJECT_NAME)
+parser.add_argument('--pixel', dest='cflags', action='append_const',
+                    const=PIXELS_DEVEL_FLAGS,
+                    help='Enable additional development flags because you\'re pixelherodev and you make bad life choices')
+parser.add_argument('--release', dest='cflags_mode', action='store_const',
+                    const=RELEASE_FLAGS, default=DEBUG_FLAGS,
+                    help='Enable release build')
+parser.add_argument('-B', dest='force_rebuild', action='store_true',
+                    help='Force rebuilding the project')
+parser.add_argument('-R', dest='dynamic', action='store_true',
+                    help='Build a dynamic executable')
+parser.add_argument('-b', dest='build_only', action='store_true',
+                    help="Don't run the executable; only build it")
+parser.add_argument('-r', dest='run_only', action='store_true',
+                    help="Don't rebuild the executable; only run it")
+parser.add_argument('-j', dest='num_threads', nargs='?',
+                    type=int, default=max(multiprocessing.cpu_count() // 2, 1),
+                    help='Number of threads to run `make` with')
+
+args = parser.parse_args()
+if args.run_only:
+    return_code = subprocess.call([BIN_DIR + '/' + BIN_NAME])
+    exit(return_code)
+
 try:
     with open("build/bump", "r") as f:
         BUMP_VER = int(f.read())
@@ -16,59 +71,12 @@ except:
     print("Failed to save tweak version")
 VERSION=str(MAJOR_VER) + '.' + str(MINOR_VER) + '.' + str(PATCH_VER)
 
-import glob
-import os
-import argparse
-from pathlib import Path
-from string import Template
-import itertools
-import subprocess
-from tempfile import NamedTemporaryFile
-import multiprocessing
 
-# should be fine to just hardcode these, IMO
-BUILD_DIR = 'build'
-BIN_DIR = BUILD_DIR
-OBJ_DIR = os.path.join(BUILD_DIR, 'obj')
-EXE_EXT = '.exe' if os.name == 'nt' else ''
-# TODO: after the jam, split compiler and VM into two executables
-# For now, we'll keep compiler source in 'src/compiler' and VM source in 'src/vm', with shared stuff in 'src/' directly.
-BIN_NAME = 'Zany80' + EXE_EXT
-
-DEFAULT_FLAGS = {'-Wall', '-pedantic', '-DSOKOL_GLCORE33', '-D_POSIX_C_SOURCE=200809L' }
-DEBUG_FLAGS = {'-g', '-Og', '-D_DEBUG'}
-# pixelherodev's personal flag set :P I'm insane, I know.
-PIXELS_DEVEL_FLAGS = { '-Werror', '-Wextra', '-Wno-error=reorder', '-Wno-error=pedantic', '-Wno-error=unused-parameter', '-Wno-error=missing-field-initializers', '-Wno-error=deprecated-declarations', '-pedantic', '-march=native', '-mtune=native', '-falign-functions=32' }
-RELEASE_FLAGS = {'-O2'}
-
-# these *do* need to be in order, so no sets!
-BASE_LDFLAGS = [ '-lX11', '-lXi', '-lXcursor', '-lGL', '-ldl', '-lstdc++', '-lm', '-pthread' ]
-DEFAULT_LDFLAGS = ['-static', '-static-libgcc' ] + BASE_LDFLAGS
-
-# parse arguments
-parser = argparse.ArgumentParser(description='Builds Zany80.')
-parser.add_argument('--pixel', dest='cflags', action='append_const',
-                    const=PIXELS_DEVEL_FLAGS,
-                    help='Enable additional development flags because you\'re pixelherodev and you make bad life choices')
-parser.add_argument('--release', dest='cflags_mode', action='store_const',
-                    const=RELEASE_FLAGS, default=DEBUG_FLAGS,
-                    help='Enable release build')
-parser.add_argument('-B', dest='force_rebuild', action='store_true',
-                    help='Force rebuilding the project')
-parser.add_argument('-r', dest='dynamic', action='store_true',
-                    help='Build a dynamic executable')
-parser.add_argument('-j', dest='num_threads', nargs='?',
-                    type=int, default=max(multiprocessing.cpu_count() // 2, 1),
-                    help='Number of threads to run `make` with')
-
-args = parser.parse_args()
 if '-O2' not in args.cflags_mode:
     VERSION = VERSION + '-' + str(BUMP_VER)
 CFLAGS = DEFAULT_FLAGS | {'-std=c99'} | args.cflags_mode | (set(itertools.chain(*args.cflags)) if args.cflags else set()) | { '-DPROJECT_VERSION=' + VERSION }
 CXXFLAGS = DEFAULT_FLAGS | {'-std=c++11'} | args.cflags_mode | (set(itertools.chain(*args.cflags)) if args.cflags else set()) | { '-DPROJECT_VERSION=' + VERSION }
-LDFLAGS = DEFAULT_LDFLAGS
-if args.dynamic:
-    LDFLAGS = BASE_LDFLAGS
+LDFLAGS = BASE_LDFLAGS
 
 class Target:
     all = {}
@@ -192,7 +200,8 @@ with Executable('Zany80') as Zany80:
     # Core Zany80 components
     Zany80.add_sources_glob('src/serial.c', 'src/z80.c', 'src/editor.c', 'src/scas.c')
     # Embedded files
-    Zany80.add_sources_glob('src/license.c', 'src/zexall.c')
+    Zany80.add_sources_glob('src/license.c', 'src/zexall.c', 'src/z80_tab.c')
+    Zany80.add_headers_glob('src/*.h')
     Zany80.add_dependencies('sokol', 'cimgui', 'stb', 'TextEditor', 'rapidxml', 'z80e')
     Zany80.add_includes('lib/')
 
@@ -209,9 +218,11 @@ CXXFLAGS+={CXXFLAGS}
 LDFLAGS+={LDFLAGS}
 INCLUDES+={includes_all}
 
-.PHONY: default all Zany80
-default: Zany80
-\t./{BIN_DIR}/Zany80
+.PHONY: default run all {PROJECT_NAME}
+default: {default}
+
+run: ./{BIN_DIR}/{PROJECT_NAME}
+\t./{BIN_DIR}/{PROJECT_NAME}
 
 all: {targets_all}
 
@@ -229,7 +240,9 @@ HEADERS={headers_all}
     targets_all=' '.join(Target.all),
     # TODO: These should be per-target (but currently cannot be because of %.o: %.c rules)
     includes_all=' '.join(sorted({'-I' + inc for inc in itertools.chain.from_iterable(tgt.includes for tgt in Target.all.values())})),
-    headers_all=' '.join(sorted(set(itertools.chain.from_iterable(tgt.headers for tgt in Target.all.values()))))
+    headers_all=' '.join(sorted(set(itertools.chain.from_iterable(tgt.headers for tgt in Target.all.values())))),
+    PROJECT_NAME = PROJECT_NAME,
+    default = PROJECT_NAME if args.build_only else 'run',
 ))
 
 def emit_target(tgt, emitted=None):
@@ -287,12 +300,8 @@ for t_name in sorted(Target.all):
     emit_target(Target.all[t_name], emitted)
 
 t = ''.join(t)
-#print('-' * 10)
-#if '-Werror' in CFLAGS:
-#    print("Makefile generated.")
-#else:
-#    print(t.rstrip())
-#print('-' * 10)
+print('-' * 10 + '\nMakefile generated.\n' + '-' * 10, flush = True)
+
 
 with open('Makefile', 'w') as f:
     f.write(t)
